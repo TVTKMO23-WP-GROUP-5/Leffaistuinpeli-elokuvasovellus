cfa5ea58b65354e66a903993ce223dd9b3199e2d
Juho Kultala <128252330+Kuljuho@users.noreply.github.com>
2024-03-28 09:36:18 +0200

    Kotisivulle sisältöä ja edustavammaksi

--- elokuva-app/src/pages/Home.js
+++ elokuva-app/src/pages/Home.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import dog1 from "../images/dog1.png";
+import dog2 from "../images/dog6.png";
+import dog3 from "../images/dog7.png";
 
 export default function Home() {
   return (
-    <div>
-      <p>moro</p>
+    <div className="container">
+      <div className="card">
+        <img src={dog3} alt="Elokuvia" />
+        <p>Hae ja arvostele elokuvia!</p>
+      </div>
+      <div className="card">
+        <img src={dog2} alt="Ryhmiin liittyminen" />
+        <p>Liity ryhmiin ja keskustele!</p>
+      </div>
+      <div className="card">
+        <img src={dog1} alt="Näytösaikojen selaaminen" />
+        <p>Selaa elokuvien näytösaikoja!</p>
+      </div>
     </div>
-  )
+  );
 }
