a58f272c9dc8c86bb220d4267a9bea1a51dbcfd4
Taneli Heikkilä <heikkilataneli@gmail.com>
2024-03-28 10:33:47 +0200

    ryhmäsivulle sisältöä

--- elokuva-app/src/pages/AllGroups.js
+++ elokuva-app/src/pages/AllGroups.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './AllGroups.css'
+import '../index.css'
 
 export default function AllGroups() {
+  const [groups, setGroups] = useState([])
   return (
-    <div>AllGroups</div>
+    <div className='container'>
+      <div className='buttons'>
+        <button className='makegroup'>Luo ryhmä</button>
+        <button className='mygroups'>Omat ryhmät</button>
+      </div>
+      <div className='info'>
+        <h2>Kaikki ryhmät</h2>
+      </div>
+      <div className='grouplist'>
+        <button className='alph_order'>A-Z ↑↓</button>
+        <p className='groupname'>Ryhmän nimi</p>
+        <p className='description'>Ryhmän kuvaus</p>
+      </div>
+      <div className='groups'>
+        <ul>
+          {groups && groups.map((group, index) => (
+            <li key={index}>{group.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
