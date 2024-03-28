b123608036f8de88fef43494f3f613a24d74cf55
Juho Kultala <128252330+Kuljuho@users.noreply.github.com>
2024-03-28 11:47:46 +0200

    Kotisivun kortit toimii linkkeinä

--- elokuva-app/src/pages/Home.js
+++ elokuva-app/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 import dog1 from "../images/dog1.png";
 import dog2 from "../images/dog6.png";
@@ -7,18 +8,18 @@ import dog3 from "../images/dog7.png";
 export default function Home() {
   return (
     <div className="container">
-      <div className="card">
+      <Link to="/Search" className="card">
         <img src={dog3} alt="Elokuvia" />
         <p>Hae ja arvostele elokuvia!</p>
-      </div>
-      <div className="card">
+      </Link>
+      <Link to="/AllGroups" className="card">
         <img src={dog2} alt="Ryhmiin liittyminen" />
         <p>Liity ryhmiin ja keskustele!</p>
-      </div>
-      <div className="card">
+      </Link>
+      <Link to="/Showtimes" className="card">
         <img src={dog1} alt="Näytösaikojen selaaminen" />
         <p>Selaa elokuvien näytösaikoja!</p>
-      </div>
+      </Link>
     </div>
   );
 }
