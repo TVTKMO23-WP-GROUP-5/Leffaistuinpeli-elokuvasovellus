caff2eebd57f9c79320884d5fc4eacfef1eaca00
Juho Kultala <128252330+Kuljuho@users.noreply.github.com>
2024-04-03 20:27:44 +0300

    Hakua aloiteltu reactin puolella

--- elokuva-app/src/pages/Search.js
+++ elokuva-app/src/pages/Search.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useUser } from "../context/UseUser";
+import axios from "axios";
 
 export default function Search() {
-  return (
-    <div>Search</div>
-  )
-}
+  const [movieData, setMovieData] = useState({ search: "" });
 
+  const handleChange = (e) => {
+    setMovieData((prevMovieData) => ({
+      ...prevMovieData,
+      [e.target.name]: e.target.value
+    }));
+    console.log(movieData);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!movieData) {
+      console.log("movieDataa ei ole");
+    }
+
+    axios
+      .post("http://localhost:3001/movies?query=", movieData)
+      .then((response) => {
+        console.log(response.data);
+        alert("Haussa tapahtuu jotain");
+      })
+      .catch((error) => {
+        alert("Haussa tapahtuu jotain negatiivista");
+      });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Hae elokuvia tai sarjoja"
+          onChange={handleChange}
+        />
+        <button type="submit">Jatka</button>
+      </form>
+    </>
+  );
+}
