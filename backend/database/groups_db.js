71ed42609fc780bd013965dd59c2ad575a57cab3
Taneli Heikkilä <heikkilataneli@gmail.com>
2024-04-05 10:40:09 +0300

    ryhmäsivua jatkettu

--- backend/database/groups_db.js
+++ backend/database/groups_db.js
@@ -2,6 +2,7 @@ const pgPool = require("./pg_connection");
 
 const sql = {
   GET_ALLGROUPS: "SELECT * FROM groups",
+  GET_GROUPSBYIDACCOUNT: "SELECT idGroup, groupname, description FROM groups JOIN groupmembers ON groups.idGroup=groupmembers.idGroup WHERE groupmembers.idAccount=$1",
   GET_GROUPBYNAME: "SELECT * FROM groups WHERE groupname=$1",
   POST_NEWGROUP: "INSERT INTO group (groupname, description, owner) VALUES ($1, $2, $3)"
 };
@@ -11,10 +12,16 @@ async function getAllGroups() {
   return result.rows;
 }
 
+async function getGroupsByIdAccount(idaccount) {
+    let result = await pgPool.query(sql.GET_GROUPSBYIDACCOUNT, [idaccount]);
+    return result.rows;
+}
+
 async function getGroupByName(groupname) {
-    let result = await pgPool.query(sql.GET_GROUPBYNAME, [groupname]);
-    return result.rows[0];
+  let result = await pgPool.query(sql.GET_GROUPBYNAME, [groupname]);
+  return result.rows[0];
 }
 
 
-module.exports = { getAllGroups, getGroupByName };
+
+module.exports = { getAllGroups, getGroupByName, getGroupsByIdAccount };
--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,14 +1,14 @@
-const { getAllGroups, getGroupByName } = require("../database/groups_db");
+const { getAllGroups, getGroupByName, getGroupsByIdAccount } = require("../database/groups_db");
 const router = require("express").Router();
 
-router.get("/", async (req, res) => {
+router.get("/allgroups", async (req, res) => {
     try {
         const groups = await getAllGroups(req.query.groupname);
 
         const filteredGroups = groups.map(group => ({
             name: group.groupname,
             description: group.description,
-            owner: group.owner
+            owner: group.ownerd
         }));
         console.log(filteredGroups);
         res.json(filteredGroups);
@@ -18,14 +18,34 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/owngroups", async (req, res) => {
+    try {
+    const owngroups = await getGroupsByIdAccount(req.query.idaccount);
+
+    const filteredGroup = owngroups.map(group => ({
+        name: group.groupname,
+        description: group.description
+    }))
+    console.log(filteredGroup);
+    res.json(filteredGroup);
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
+
 router.get("/groupname", async (req, res) => {
     try {
     const group = await getGroupByName(req.query.groupname);
 
-    const filteredGroup = group.map(group => ({
+    if (!group) {
+        return res.status(404).send('Group not found');
+    }
+
+    const filteredGroup = {
         name: group.groupname,
         description: group.description
-    }))
+    };
+
     console.log(filteredGroup);
     res.json(filteredGroup);
     } catch (error) {
