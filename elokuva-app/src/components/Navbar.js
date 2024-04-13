d84a874eb8b969e22b95b667694179b7146561dc
Juho Kultala <128252330+Kuljuho@users.noreply.github.com>
2024-04-13 03:01:38 +0300

    Burgervalikko, tumma teema

--- elokuva-app/src/components/Navbar.js
+++ elokuva-app/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo1 from "../images/logo1.png";
+import logo2 from "../images/logo2.png";
 import { Link } from "react-router-dom";
 import { useUser } from "../context/UseUser";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faMagnifyingGlass, faCalendarDays, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
-import axios from "axios";
+import { useTheme } from "../context/ThemeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from 'axios';
+import { faHouse, faMagnifyingGlass, faCalendarDays, faPeopleGroup, faSun, faMoon, faGear, faRightFromBracket, faStar, faUserGroup, faGroupArrowsRotate } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
   const { user } = useUser();
@@ -13,8 +15,9 @@ export default function Navbar() {
   const [burger, setBurger] = useState("Burger unclicked");
   const [menu, setMenu] = useState("Menu hidden");
   const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const { theme, changeTheme } = useTheme();
 
-  console.log(user)
+  const logos = theme === 'dark'? logo2 : logo1;
 
   useEffect(() => {
     axios
@@ -41,22 +44,22 @@ export default function Navbar() {
       setBurger("Burger unclicked");
       setIsMenuClicked(false);
     };
-  
+
     if (menu === "visible") {
-      document.addEventListener('click', handleOutsideClick);
+      document.addEventListener("click", handleOutsideClick);
     } else {
-      document.removeEventListener('click', handleOutsideClick);
+      document.removeEventListener("click", handleOutsideClick);
     }
-  
+
     return () => {
-      document.removeEventListener('click', handleOutsideClick);
+      document.removeEventListener("click", handleOutsideClick);
     };
   }, [menu]);
 
   return (
     <nav>
       <div className="logo-container">
-        <img src={logo1} alt="Logo" />
+        <img src={logos} alt="Logo" />
       </div>
 
       <div className={`burger-menu ${burger}`} onClick={updateMenu}>
@@ -67,65 +70,73 @@ export default function Navbar() {
       <div className={`menu ${menu}`}>
         <ul id="menu">
           <li>
-            <Link to="/"><FontAwesomeIcon icon={faHouse} />Etusivu</Link>
+            <Link to="/"><FontAwesomeIcon icon={faHouse} className="fa-icon"/><span className="link-text">Etusivu</span></Link>
           </li>
           <li>
-            <Link to="/search"><FontAwesomeIcon icon={faMagnifyingGlass} />Haku</Link>
+            <Link to="/search"><FontAwesomeIcon icon={faMagnifyingGlass} className="fa-icon"/><span className="link-text">Haku</span></Link>
           </li>
           <li>
-            <Link to="/showtimes"><FontAwesomeIcon icon={faCalendarDays} />Näytösajat</Link>
+            <Link to="/showtimes">
+              <FontAwesomeIcon icon={faCalendarDays} className="fa-icon"/>
+              <span className="link-text">Näytösajat</span>
+            </Link>
           </li>
           <li>
-            <Link to="/allgroups"><FontAwesomeIcon icon={faPeopleGroup} />Ryhmät</Link>
+            <Link to="/allgroups">
+              <FontAwesomeIcon icon={faPeopleGroup} className="fa-icon"/>
+              <span className="link-text">Ryhmät</span>
+            </Link>
           </li>
           <li>
-            <Link to="/search">Haku</Link>
+          <Link to="/owngroups"><FontAwesomeIcon icon={faUserGroup} className="fa-icon"/><span className="link-text">Omat ryhmät</span></Link>
           </li>
+          {isAdmin && (
+            <>
+              <li>
+                <Link to="/adminpage"><FontAwesomeIcon icon={faGroupArrowsRotate} className="fa-icon"/><span className="link-text">Ryhmän ylläpito</span></Link>
+              </li>
+            </>
+          )}
           <li>
-            <Link to="/search">Haku</Link>
-          </li>
-          <li>
-            <Link to="/search">Haku</Link>
-          </li>
-          <li>
-            <Link to="/search">Haku</Link>
+            <Link to="/search"><FontAwesomeIcon icon={faStar} className="fa-icon" /><span className="link-text">Arvostelut</span></Link>
           </li>
           {user && (
             <>
               <li>
-                <Link to="/userpage" className="navButton">
-                  Omat tiedot
-                </Link>
+                <Link to="/userpage"><FontAwesomeIcon icon={faGear} className="fa-icon"/><span className="link-text">Omat tiedot</span></Link>
               </li>
             </>
           )}
-          {isAdmin && (
-            <>
-              <li>
-                <Link to="/adminpage">Ryhmän ylläpito</Link>
-              </li>
-            </>
-          )}
-          {user === null && <Link to="/login">Kirjaudu sisään</Link>}
-          {user && <Link to="/logout">Kirjaudu ulos</Link>}
+          <li>
+          {user === null && <Link to="/login"><FontAwesomeIcon icon={faRightFromBracket}  className="fa-icon"/><span className="link-text">Kirjaudu sisään</span></Link>}
+          {user && <Link to="/logout"><FontAwesomeIcon icon={faRightFromBracket} className="fa-icon"/><span className="link-text">Kirjaudu ulos</span></Link>}
+          </li>
+          <li>
+            <Link to="/search">Haku</Link>
+          </li>
+          <li>
+            <Link to="/search">Haku</Link>
+          </li>
         </ul>
       </div>
 
       <div className="button-container">
-        <ul id="buttons">
+        <ul className="buttons">
           <li>
             <Link to="/search" className="navButton">
-              Haku
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="fa-icon"/><span className="link-text">Haku</span>
             </Link>
           </li>
           <li>
             <Link to="/showtimes" className="navButton">
-              Näytösajat
+            <FontAwesomeIcon icon={faCalendarDays} className="fa-icon"/>
+              <span className="link-text">Näytösajat</span>
             </Link>
           </li>
           <li>
             <Link to="/allgroups" className="navButton">
-              Ryhmät
+            <FontAwesomeIcon icon={faPeopleGroup} className="fa-icon"/>
+              <span className="link-text">Ryhmät</span>
             </Link>
           </li>
         </ul>
@@ -136,17 +147,26 @@ export default function Navbar() {
       </div>
 
       <div className="sign-container">
-        {user === null && (
+      {user === null && (
           <Link to="/login" className="signButton">
-            Kirjaudu sisään
+            <FontAwesomeIcon icon={faRightFromBracket} className="fa-icon"/><span className="login">Kirjaudu sisään</span>
+            <span className="login-small">Kirjaudu</span>
           </Link>
         )}
         {user && (
           <Link to="/logout" className="signButton">
-            Kirjaudu ulos
+            <FontAwesomeIcon icon={faRightFromBracket} className="fa-icon"/><span className="login">Kirjaudu ulos</span>
+            <span className="login-small">Kirjaudu</span>
           </Link>
         )}
-      </div>
+        </div>
+        <div className="theme" onClick={changeTheme}>
+          {theme === "dark" ? (
+            <FontAwesomeIcon icon={faMoon} size="2x" className="theme-icon"/>
+          ) : (
+            <FontAwesomeIcon icon={faSun} size="2x" className="theme-icon"/>
+          )}
+        </div>
     </nav>
   );
 }
