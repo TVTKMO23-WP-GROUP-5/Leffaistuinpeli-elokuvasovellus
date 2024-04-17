e7a53fc07b36390bf5023eb05b907c6083142de8
Taneli Heikkilä <heikkilataneli@gmail.com>
2024-04-17 20:38:45 +0300

    omat suosikit tulee nyt jaoteltuna sarjoihin ja elokuviin, ryhmän suosikit vielä vaiheessa sekä omien suosikkien responsiivisuus

--- backend/database/favorite_db.js
+++ backend/database/favorite_db.js
@@ -1,20 +1,20 @@
 const pgPool = require("./pg_connection");
 
 const sql = {
-    POST_FAVORITE: "INSERT INTO favorites (idmovie, username) VALUES ($1, $2)",
-    POST_GROUP_FAVORITE: "INSERT INTO groupmovies (idmovie, groupname) VALUES ($1, $2)",
-    GET_OWN_FAVORITES: "SELECT idmovie FROM favorites WHERE username=$1",
-    GET_GROUP_FAVORITES: "SELECT idmovie FROM groupmovies WHERE groupname=$1",
+    POST_FAVORITE: "INSERT INTO favorites (idmovie, username, media_type) VALUES ($1, $2, $3)",
+    POST_GROUP_FAVORITE: "INSERT INTO groupmovies (idmovie, groupname, media_type) VALUES ($1, $2)",
+    GET_OWN_FAVORITES: "SELECT idmovie, media_type FROM favorites WHERE username=$1",
+    GET_GROUP_FAVORITES: "SELECT idmovie, media_type FROM groupmovies WHERE groupname=$1",
     CHECK_OWN_FAVORITES: "SELECT EXISTS ( SELECT 1 FROM favorites WHERE idmovie=$1 AND username=$2 )",
     CHECK_GROUP_FAVORITES: "SELECT EXISTS ( SELECT 1 FROM groupmovies WHERE idmovie=$1 AND groupname=$2 )"
 };
 
-async function addFavorite(idmovie, username) {
-  await pgPool.query(sql.POST_FAVORITE, [idmovie, username]);
+async function addFavorite(idmovie, username, media_type) {
+  await pgPool.query(sql.POST_FAVORITE, [idmovie, username, media_type]);
 }
 
-async function addGroupFavorite(idmovie, groupname) {
-    await pgPool.query(sql.POST_GROUP_FAVORITE, [idmovie, groupname]);
+async function addGroupFavorite(idmovie, groupname, media_type) {
+    await pgPool.query(sql.POST_GROUP_FAVORITE, [idmovie, groupname, media_type]);
 }
 
 async function getOwnFavorites(username) {
