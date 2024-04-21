4de1f815f815c7aad51e02d1e9842514fca1e24f
Jaakko Mattila <t3maja06@students.oamk.fi>
2024-04-21 12:59:53 +0300

    Arvostelusivu valmis

--- elokuva-app/src/context/UserProvider.js
+++ elokuva-app/src/context/UserProvider.js
@@ -4,111 +4,113 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { jwtToken } from '../components/Auth_signals'
 
-export default function UserProvider({children}) {
-    const [user, setUser] = useState(null)
-    const [isAdmin, setIsAdmin] = useState(false);
-    const [registerData, setRegisterData] = useState() // Jaakon lisäys 27.3. rekisteröintiä varten
-    const [movieData, setMovieData] = useState()
-    const [moviePick, setMoviePick] = useState([])
-    const [groups, setGroups] = useState([])
-    const [userGroups, setUserGroups] = useState([])
-    const [isHome, setIsHome] = useState(false)
-    const [loading, setLoading] = useState(false)
-    const [movies, setMovies] = useState([])
-    const [groupMembers, setGroupMembers] = useState({})
-    const [ratingsList, setRatingsList] = useState([])
-    const navigate = useNavigate()
+export default function UserProvider({ children }) {
+  const [user, setUser] = useState(null)
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [registerData, setRegisterData] = useState() // Jaakon lisäys 27.3. rekisteröintiä varten
+  const [movieData, setMovieData] = useState()
+  const [moviePick, setMoviePick] = useState([])
+  const [groups, setGroups] = useState([])
+  const [userGroups, setUserGroups] = useState([])
+  const [isHome, setIsHome] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [movies, setMovies] = useState([])
+  const [groupMembers, setGroupMembers] = useState({})
+  const [ratingsList, setRatingsList] = useState([])
+  const navigate = useNavigate()
 
-    const login = async(uname,password) => {
-        axios.post('http://localhost:3001/auth/login',
-            {username: uname, password: password})
-            .then(resp => {
-                setUser(uname) // jaakko muokkas, että tässä setUseriin menee vain uname, aiemmin {username: uname}, mutta se aiheutti ongelmia adminiin. 
-                jwtToken.value = resp.data.jwtToken
-                sessionStorage.setItem('username', uname) 
-                navigate('/userpage')
-                alert("Kirjautuminen onnistui!")
-            })
-            .catch(err => console.log(err.message))
+  const login = async (uname, password) => {
+    axios.post('http://localhost:3001/auth/login',
+      { username: uname, password: password })
+      .then(resp => {
+        setUser(uname) // jaakko muokkas, että tässä setUseriin menee vain uname, aiemmin {username: uname}, mutta se aiheutti ongelmia adminiin. 
+        jwtToken.value = resp.data.jwtToken
+        sessionStorage.setItem('username', uname)
+        navigate('/userpage')
+        alert("Kirjautuminen onnistui!")
+      })
+      .catch(err => console.log(err.message))
+  }
+
+  // Asettaa groupMemberseihin,jos käyttäjä on adminina niin tietoa. 
+  useEffect(() => {
+    if (user !== null) {
+      axios.post('http://localhost:3001/getmembers', { username: user })
+        .then(response => {
+          setGroupMembers(response.data)
+          console.log(response.data.application)
+        })
+        .catch(error => {
+          console.error("Fetching failed", error)
+        })
     }
+  }, [user]);
 
-    // Asettaa groupMemberseihin,jos käyttäjä on adminina niin tietoa. 
-    useEffect(() => {
-      if (user !== null) {
-          axios.post('http://localhost:3001/getmembers', {username: user})
-              .then(response => {
-                  setGroupMembers(response.data)
-                  console.log(response.data.application)
-              })
-              .catch(error => {
-                  console.error("Fetching failed", error)
-              })
-      }
-    }, [user]);
+  // Asettaa kaikki arvostelut ratingsListaan
+  useEffect(() => {
+    axios.get("http://localhost:3001/rating/getrating")
+      .then((response) => {
+        setRatingsList(response.data)
+      })
+      .catch((error) => {
+        console.error("Error adding favorite:", error.response.data);
+        alert("Virhe arvostelujen lataamisessa.");
+      })
+  }, [])
 
-    // Asettaa kaikki arvostelut ratingsListaan
-    useEffect(() => {
-      axios.get("http://localhost:3001/rating/getrating")
-          .then((response) => {
-            setRatingsList(response.data)
-          })
-          .catch((error) => {
-            console.error("Error adding favorite:", error.response.data);
-            alert("Virhe arvostelujen lataamisessa.");
-          })
-  })
+  useEffect(() => {
+    const storedUser = sessionStorage.getItem("username");
+    if (storedUser === "null") {
+      setUser(null);
+    } else {
+      setUser(storedUser)
+    }
+  }, [setUser]);
 
-    useEffect(() => {
-        const storedUser = sessionStorage.getItem("username");
-        if (storedUser === "null") {
-          setUser(null);
-        } else {
-          setUser(storedUser)
-        }
-      }, [setUser]);
-    
-    useEffect(() => {
-        const storedMoviePick = sessionStorage.getItem("moviePick");
-        if (Array.isArray(storedMoviePick)) {
-          setMoviePick((storedMoviePick));
-        } else {
-            setMoviePick([])
-        }
-      }, [setMoviePick]);
+  useEffect(() => {
+    const storedMoviePick = sessionStorage.getItem("moviePick");
+    if (Array.isArray(storedMoviePick)) {
+      setMoviePick((storedMoviePick));
+    } else {
+      setMoviePick([])
+    }
+  }, [setMoviePick]);
 
-    useEffect(() => {
-        axios
-          .post("http://localhost:3001/getmembers/checkowner", { username: user })
-          .then((response) => {
-            setIsAdmin(response.data);
-          })
-          .catch((error) => {
-            console.error("Fetching failed", error);
-          });
-      }, [user]);
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/getmembers/checkowner", { username: user })
+      .then((response) => {
+        setIsAdmin(response.data);
+      })
+      .catch((error) => {
+        console.error("Fetching failed", error);
+      });
+  }, [user]);
 
-    useEffect(() => {
-      if (user) {
-        const username = sessionStorage.getItem('username')
-        axios
-          .get(`http://localhost:3001/groups/owngroups?username=${username}`)
-          .then((response) => {
-            setUserGroups(response.data);
-            console.log(userGroups)
-          })
-          .catch((error) => {
-            console.error("Fetching failed", error);
-          });
-        }
-      }, [user]);
+  useEffect(() => {
+    if (user) {
+      const username = sessionStorage.getItem('username')
+      axios
+        .get(`http://localhost:3001/groups/owngroups?username=${username}`)
+        .then((response) => {
+          setUserGroups(response.data);
+          console.log(userGroups)
+        })
+        .catch((error) => {
+          console.error("Fetching failed", error);
+        });
+    }
+  }, [user]);
 
 
-    return (
-        <UserContext.Provider value={{user,setUser,registerData,setRegisterData,
-          movieData,setMovieData,moviePick,setMoviePick, isHome, setIsHome,
-          loading,setLoading,movies,setMovies,groups,setGroups,userGroups,setUserGroups,login,
-          isAdmin, setIsAdmin, groupMembers, setGroupMembers, ratingsList, setRatingsList}}>
-            { children }
-        </UserContext.Provider>
-    )
+  return (
+    <UserContext.Provider value={{
+      user, setUser, registerData, setRegisterData,
+      movieData, setMovieData, moviePick, setMoviePick, isHome, setIsHome,
+      loading, setLoading, movies, setMovies, groups, setGroups, userGroups, setUserGroups, login,
+      isAdmin, setIsAdmin, groupMembers, setGroupMembers, ratingsList, setRatingsList
+    }}>
+      {children}
+    </UserContext.Provider>
+  )
 }
--- elokuva-app/src/pages/Ratings.js
+++ elokuva-app/src/pages/Ratings.js
@@ -4,121 +4,202 @@ import axios from 'axios'
 import { useUser } from '../context/UseUser'
 
 export default function Ratings() {
-const { ratingsList, setRatingsList, user} = useUser()
-const [ showRatings, setShowRatings ] = useState([])
-const [ sort, setSort] = useState([])
-const [ sortType, setSortType] = useState("name")
-const [ isChecked, setIsChecked ] = useState(false)
+    const { ratingsList, setRatingsList, user } = useUser()
+    const [showRatings, setShowRatings] = useState([])
+    const [sort, setSort] = useState([])
+    const [sortType, setSortType] = useState("name")
+    const [isChecked, setIsChecked] = useState(false)
+    const [filter, setFilter] = useState("")
+    const [page, setPage] = useState(1)
+    const [itemsPerPage, setItemsPerPage] = useState(10)
+    const [isButton, setIsButton] = useState(true)
 
-useEffect(() => {
-    axios.get("http://localhost:3001/rating/getrating")
-        .then((response) => {
-          setRatingsList(response.data)
-        })
-        .catch((error) => {
-          console.error("Error adding favorite:", error.response.data);
-          alert("Virhe arvostelujen lataamisessa.");
-        })
-})
+    useEffect(() => {
+        axios.post("http://localhost:3001/rating/ratedmoviedata", { data: ratingsList })
+            .then((response) => {
+                setShowRatings(response.data)
+                setSort(response.data)
+            })
+            .catch((error) => {
+                console.error("Error adding favorite:", error.response.data);
+                alert("Virhe arvostelujen lataamisessa.");
+            })
+    }, [user, setShowRatings, setSort])
 
-useEffect(() => {
-    axios.post("http://localhost:3001/rating/ratedmoviedata", {data: ratingsList})
-        .then((response) => {
-          setShowRatings(response.data)
-          setSort(response.data)
-        })
-        .catch((error) => {
-          console.error("Error adding favorite:", error.response.data);
-          alert("Virhe arvostelujen lataamisessa.");
-        })
-        console.log(ratingsList)
-}, [user, setShowRatings])
-  
-const sortMovies = (sorting, check) => {
-    console.log(sorting, check)
-    if (sorting === "name"){
-        const sortedRatings = [...showRatings].sort((a, b) => {
-            const titleA = a.title ? a.title.toLowerCase() : a.name.toLowerCase();
-            const titleB = b.title ? b.title.toLowerCase() : b.name.toLowerCase();
-            if (check){
-                return titleB.localeCompare(titleA);
-            } else if (!check) {
-                return titleA.localeCompare(titleB);
-            }
-            
-        })
-        setSort(sortedRatings)
-    } else if (sorting ==="stars"){
-        const sortedRatings = [...showRatings].sort((a, b) => {
-            const starA = parseInt(a.stars);
-            const starB = parseInt(b.stars);
-            if (check){
-                return starB - starA
-            } else if (!check) {
-                return starA - starB
-            }
-            
-        })
-        setSort(sortedRatings)
-    } else if (sorting === "user") {
-        const sortedRatings = [...showRatings].sort((a, b) => {
-            const userA = a.rater.toLowerCase()
-            const userB = b.rater.toLowerCase()
-            if (check){
-                return userB.localeCompare(userA)
-            } else if (!check) {
-                return userA.localeCompare(userB)
-            }  
-        })
-        setSort(sortedRatings)
+    const sortMovies = (sorting, check) => {
+        setFilter("")
+        console.log(sorting, check)
+        if (sorting === "name") {
+            const sortedRatings = [...showRatings].sort((a, b) => {
+                const titleA = a.title ? a.title.toLowerCase() : a.name.toLowerCase();
+                const titleB = b.title ? b.title.toLowerCase() : b.name.toLowerCase();
+                if (check) {
+                    return titleB.localeCompare(titleA);
+                } else if (!check) {
+                    return titleA.localeCompare(titleB);
+                }
+
+            })
+            setSort(sortedRatings)
+        } else if (sorting === "stars") {
+            const sortedRatings = [...showRatings].sort((a, b) => {
+                const starA = parseInt(a.stars);
+                const starB = parseInt(b.stars);
+                if (check) {
+                    return starB - starA
+                } else if (!check) {
+                    return starA - starB
+                }
+
+            })
+            setSort(sortedRatings)
+        } else if (sorting === "user") {
+            const sortedRatings = [...showRatings].sort((a, b) => {
+                const userA = a.rater.toLowerCase()
+                const userB = b.rater.toLowerCase()
+                if (check) {
+                    return userB.localeCompare(userA)
+                } else if (!check) {
+                    return userA.localeCompare(userB)
+                }
+            })
+            setSort(sortedRatings)
+        } else if (sorting === "average") {
+            const sortedRatings = [...showRatings].sort((a, b) => {
+                const averageA = parseFloat(averages[a.title ? a.title : a.name]);
+                const averageB = parseFloat(averages[b.title ? b.title : b.name]);
+                return check ? averageB - averageA : averageA - averageB;
+            });
+            setSort(sortedRatings);
+        }
     }
-}
 
-return (
-    <div>
-        <div className = "choises">
-            <select name = "sort" onChange = {(e) =>{
-                setSortType(e.target.value) 
-                sortMovies(e.target.value, isChecked)
-            }}>
-                <option disabled selected>Järjestä</option>
-                <option value = "name">Elokuvan nimi</option>
-                <option value = "stars">Tähdet</option>
-                <option value = "user">Arvostelija</option>
-            </select>
-            <label class="toggle-switch">
-                <input type="checkbox" checked = {isChecked} onChange={(e) => {
-                    setIsChecked(e.target.checked)
-                    sortMovies(sortType, e.target.checked)
-                }}/>
-                  <span className="slider">
-                    <span className="arrow-up">&#9650;</span>
-                    <span className="arrow-down">&#9660;</span>
-                </span>
-            </label>
-        </div>
-        <div className = "ratingslist">
-            {sort &&
-            sort.map((movie, index) => (
-                <div className = "movieRatings">
-                    <img
-                        src={
-                        "https://image.tmdb.org/t/p/w342/" + movie.poster_path
-                        }
-                        alt="Movie poster"
-                    />
-                    <div className = "movieDetails">
-                        <p><strong>{movie.title ? movie.title : movie.name}</strong> {movie.media_type === "movie" ? (<span>(elokuva)</span>) : movie.media_type === "tv" && (<span>(tv-sarja)</span>) }</p>
-                        <p>
-                            {Array.from({ length: movie.stars }, (_, index) => (
-                                <span key={index} className="ratingstars">&#9733;</span>
-                            ))}
-                        </p>
-                        <p>Käyttäjä <strong>{movie.rater}:</strong>  {movie.description}</p>
-                    </div>
+    const clickPick = (name) => {
+        setFilter(name)
+        setPage(1)
+        setIsButton(false)
+        setItemsPerPage(1000)
+    }
+
+    const handleClickNext = () => {
+        setPage(prevPage => prevPage + 1)
+        console.log("höÖ", endIndex, sort.length)
+    };
+
+    const startIndex = (page - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+
+    const handleClickPrevious = () => {
+        setPage(prevPage => Math.max(prevPage - 1, 1))
+        console.log("höÖ", endIndex, sort.length)
+    };
+
+    const topFiveList = () => {
+        const voteAverage = {}
+        sort.forEach((movie, index) => {
+            if (voteAverage[movie.title ? movie.title : movie.name]) {
+                voteAverage[movie.title ? movie.title : movie.name].push(movie.stars);
+            } else {
+                voteAverage[movie.title ? movie.title : movie.name] = [movie.stars];
+            }
+        })
+        const averages = {}
+        Object.keys(voteAverage).forEach(movieName => {
+            const starsArray = voteAverage[movieName]
+            const sum = starsArray.reduce((acc, star) => acc + star, 0)
+            const average = sum / starsArray.length
+            averages[movieName] = average.toFixed(2)
+        })
+
+        const averagesArray = Object.entries(averages);
+
+        averagesArray.sort((a, b) => b[1] - a[1]);
+
+        const sortedAverages = Object.fromEntries(averagesArray);
+
+        return sortedAverages;
+    }
+    const averages = topFiveList()
+    return (
+        <div>
+            <div className="choises">
+                <select name="sort" onChange={(e) => {
+                    setSortType(e.target.value)
+                    sortMovies(e.target.value, isChecked)
+                    setIsButton(true)
+                    setItemsPerPage(10)
+                }}>
+                    <option disabled selected>Järjestä</option>
+                    <option value="name">Elokuvan nimi</option>
+                    <option value="stars">Tähdet</option>
+                    <option value="average">Tähdet (keskiarvo)</option>
+                    <option value="user">Arvostelija</option>
+
+                </select>
+                <label className="toggle-switch">
+                    <input type="checkbox" checked={isChecked} onChange={(e) => {
+                        setIsChecked(e.target.checked)
+                        sortMovies(sortType, e.target.checked)
+                        setIsButton(true)
+                        setItemsPerPage(10)
+                    }} />
+                    <span className="slider">
+                        <span className="arrow-up">&#9650;</span>
+                        <span className="arrow-down">&#9660;</span>
+                    </span>
+                </label>
+                <div>
+                    <input type="text" placeholder="Hakusana" onChange={(e) => {
+                        setFilter(e.target.value)
+                        setIsButton(false)
+                        setItemsPerPage(1000)
+                    }} />
                 </div>
-            ))}
+            </div>
+            <div className="changePage">
+                {isButton && page > 1 && <button onClick={handleClickPrevious} disabled={page === 1}>Edellinen</button>}
+                <div className="page-number">sivu {page}</div>
+                {isButton && endIndex < sort.length && <button onClick={handleClickNext}>Seuraava</button>}
+            </div>
+            <div className="ratingslist">
+                {sort &&
+                    sort
+                        .filter(movie =>
+                            filter === "" ||
+                            movie.title === filter ||
+                            movie.name === filter ||
+                            movie.rater === filter ||
+                            (movie.title ? movie.title.toLowerCase().includes(filter.toLowerCase()) : false) ||
+                            (movie.name ? movie.name.toLowerCase().includes(filter.toLowerCase()) : false)
+                        )
+                        .slice(startIndex, endIndex)
+                        .map((movie, index) => (
+                            <div className="movieRatings" key={index}>
+                                <img
+                                    src={"https://image.tmdb.org/t/p/w342/" + movie.poster_path}
+                                    alt="Movie poster"
+                                    onClick={() => clickPick(movie.title ? movie.title : movie.name)}
+                                />
+                                <div className="movieDetails">
+                                    <p><strong>{movie.title ? movie.title : movie.name}</strong> {movie.media_type === "movie" ? (<span>(elokuva)</span>) : movie.media_type === "tv" && (<span>(tv-sarja)</span>)}</p>
+                                    <p>
+                                        {Array.from({ length: movie.stars }, (_, index) => (
+                                            <span key={index} className="ratingstars">&#9733;</span>
+                                        ))}
+                                        <span className="average">(keskiarvo: {averages[movie.title ? movie.title : movie.name]})</span>
+                                    </p>
+                                    <p>Käyttäjä <span className="raterName" onClick={() => clickPick(movie.rater)}><strong>{movie.rater}:</strong></span></p>
+                                    <p>{movie.description}</p>
+                                </div>
+                            </div>
+                        ))}
+
+            </div>
+            <div className="changePage">
+                {isButton && page > 1 && <button onClick={handleClickPrevious} disabled={page === 1}>Edellinen</button>}
+                <div className="page-number">sivu {page}</div>
+                {isButton && endIndex < sort.length && <button onClick={handleClickNext}>Seuraava</button>}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
