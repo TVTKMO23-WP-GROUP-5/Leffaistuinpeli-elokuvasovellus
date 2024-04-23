08508dd3e0758049b7dd0653d51679be54ad45eb
Taneli Heikkilä <heikkilataneli@gmail.com>
2024-04-23 18:19:30 +0300

    ryhmän suosikkiaikojen lisäämis-ominaisuus toimii

--- backend/routes/groupST.js
+++ backend/routes/groupST.js
@@ -1,4 +1,4 @@
-const { addShowtime } = require("../database/groupST_db");
+const { addShowtime, getGroupShowtimes } = require("../database/groupST_db");
 
 const router = require("express").Router();
 
@@ -11,5 +11,17 @@ router.post("/addshowtime", async (req, res) => {
       res.status(500).json({message: error.message})
     }
   });
+
+  router.get("/getgrouptimes", async (req,res) => {
+    try {
+        const gname = req.query.groupname;
+        const showtimeData = await getGroupShowtimes(gname)
+        res.json(showtimeData)
+    } catch(error) {                                                                                    // jotka ovat samassa ryhmässä kuin admin
+        console.error(error);
+        res.status(500).send('Sever error');
+    }  
+})
+
 
 module.exports = router;
